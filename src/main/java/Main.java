7820ce5a1fb107b1beca86d0edecf9690546cef9
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-19 12:31:55 -0300

    add Main start app v1.0.0 and config files

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+//import Controller.PersonController;
+import View.App;
+
+public class Main {
+    public static void main(String[] args) {
+        // v2.0.0
+        new App().setVisible(true);
+        // v1.0.0
+        //PersonController.iniciarApp();
+    }
+}
